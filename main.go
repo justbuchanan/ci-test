3ac196114c5ee384599ef6530589f57c884f4c57
Justin Buchanan <justbuchanan@gmail.com>
2017-10-19 22:16:12 -0700

    cmdline args

--- main.go
+++ main.go
@@ -20,6 +20,10 @@ func main() {
 	// Follow these directions to create a token:
 	// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
 	var token = flag.String("token", "", "GitHub api token. Should be restricted to repo:status scope.")
+	var targetUrl = flag.String("target_url", "https://github.com", "Url that this status should redirect to.")
+	var statusCtxt = flag.String("context", "status", "Unique string identifier for this status. Something like 'compile', 'test', 'deploy'.")
+	var desc = flag.String("description", "", "Description of the test, etc.")
+	var state = flag.String("state", "error", "Repo status. Can be one of 'error', 'failure', 'pending', 'success'")
 	flag.Parse()
 
 	if *token == "" {
@@ -32,16 +36,11 @@ func main() {
 
 	client := github.NewClient(tc)
 
-	target := "https://mytestfailure.com"
-	id := "mytest"
-	state := "pending"
-	desc := "Neutrinos must have bombarded the RAM and caused a memory error."
-
 	status := github.RepoStatus{
-		TargetURL:   &target,
-		State:       &state,
-		Description: &desc,
-		Context:     &id,
+		TargetURL:   &*targetUrl,
+		State:       &*state,
+		Description: &*desc,
+		Context:     &*statusCtxt,
 	}
 	ss, resp, err := client.Repositories.CreateStatus(ctx, "justbuchanan", "ci-test", "8827dd06dfdf43f389b39b90f3d33d0e303bfc2d", &status)
 	checkErr(err)
