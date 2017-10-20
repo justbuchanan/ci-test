2291214549a1f3d45c13da95e6e5e6210e2ce6bc
Justin Buchanan <justbuchanan@gmail.com>
2017-10-19 21:47:05 -0700

    added auth

--- main.go
+++ main.go
@@ -3,24 +3,48 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 	"log"
 	"os"
 )
 
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	// Follow these directions to create a token:
+	// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+	var token = flag.String("token", "", "GitHub api token. Should be restricted to repo:status scope.")
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("Invalid GitHub status token")
+	}
+
 	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: *token})
+	tc := oauth2.NewClient(ctx, ts)
 
-	client := github.NewClient(nil)
+	client := github.NewClient(tc)
 
-	orgs, _, err := client.Organizations.List(ctx, "justbuchanan", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+	checkErr(err)
+	printJson(repos)
+}
 
-	b, err := json.Marshal(orgs)
-	if err != nil {
-		log.Fatal(err)
-	}
+func printJson(x interface{}) {
+	b, err := json.Marshal(x)
+	checkErr(err)
 	os.Stdout.Write(b)
+}
+
+func listOrgs(ctx context.Context, client github.Client) {
+	orgs, _, err := client.Organizations.List(ctx, "justbuchanan", nil)
+	checkErr(err)
+	printJson(orgs)
 }
